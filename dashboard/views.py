817fc50aac07db526eec280c2625471e9a75f16f
nesrine <nesrine.fezzani@esprit.tn>
2026-05-05 11:48:50 +0100

    UI : Intégration du template Dashboard complet avec Bootstrap 5 et icônes

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Session, RawMeasure
 
-# Create your views here.
+def home(request):
+    # On récupère quelques stats de la base de données
+    total_sessions = Session.objects.count()
+    derniere_mesure = RawMeasure.objects.order_by('-timestamp').first()
+
+    context = {
+        'total_sessions': total_sessions,
+        'derniere_mesure': derniere_mesure,
+        'status_ia': "Actif" # Juste pour le test
+    }
+    return render(request, 'dashboard/index.html', context)
